b1e88ef39e91fd02b58f4ff107d17e4bd85da3f2
Dani Ballesteros <pangeanoteniafronteras@gmail.com>
2018-06-16 16:25:16 +0200

    test-gitignore2

--- app/core/config.php
+++ app/core/config.php
@@ -12,7 +12,7 @@
     define('DB_HOST','localhost');
     define('DB_USER','root');
     define('DB_PASS','*****');
-    define('DB_NAME','db_name');
+    define('DB_NAME','dbname');
     define('DB_OPTIONS',array(
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
     ));
